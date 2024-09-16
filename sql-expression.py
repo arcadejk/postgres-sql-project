be86b15dd09aa9c9321fb1062ad040008fd0190e
Arcade Kakpo <arcadej@hotmail.fr>
2024-09-16 19:30:51 +0000

    Use sqlalchemy and orm to display data from the database

--- sql-expression.py
+++ sql-expression.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
 
 # executing the instructions from our localhost "chinook" db
 db = create_engine("postgresql:///chinook")
